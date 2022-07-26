1d4cd3c46723476bcbc216db2f85a3ef12d803ed
Parra <andresparraarze@gmail.com>
2022-07-26 13:24:08 -0400

    fixes and creation of home routes, public  and css to be changed

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(require('./controllers/'));
 
 
 app.listen(PORT, () => {
